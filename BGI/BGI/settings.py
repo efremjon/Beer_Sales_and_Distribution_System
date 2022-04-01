81b99f747907ca2169e62a49d314a331ee7d78c0
efremjon <efremyohanis111@gmial.com>
2022-04-01 11:21:17 -0700

    static file added and login and registration page setuped

--- BGI/BGI/settings.py
+++ BGI/BGI/settings.py
@@ -113,6 +113,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT=os.path.join(BASE_DIR,'asset')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
