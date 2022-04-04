201f4e4f0700f8b0d2de8ab5335abdd1929a1e61
efremjon <efremyohanis111@gmial.com>
2022-04-04 15:59:39 -0700

    Customer Order page integrated with django and can you access from EFREM_YOHANIS branch

--- BGI/Coustomer/urls.py
+++ BGI/Coustomer/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns =[
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('Customer_Order/',views.Cust_order,name='Cust_order'),
 ]
--- BGI/Coustomer/views.py
+++ BGI/Coustomer/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'Coustomer/home.html',{})
+    return render(request,'Customer/home.html',{})
+
+def Cust_order(request):
+    return render(request,'Customer/customer_order.html',{})
+
 
 def login(request):
     return render(request,'')
