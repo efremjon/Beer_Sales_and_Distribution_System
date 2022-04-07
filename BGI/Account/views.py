c2f695e242ea0493693f79c3955d147d1ac0829f
iphadan <ephadaniel177@gmail.com>
2022-04-07 05:52:19 +0300

    cust_ord_edited1

--- BGI/Account/views.py
+++ BGI/Account/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from Company.models import Customer ,Agent ,Finance ,Agent_finance,Agent_store_manager,Store_manager,Driver
 
 # Create your views here.
 #based on the request url ,we should decied who they are 
+#login and session setting function and redirect to dashboard 
 def login(request):
     if request.method == 'POST':
         if request.POST['password']!="" and request.POST['name']!="":
@@ -11,9 +12,11 @@ def login(request):
             password=request.POST['password']
             user=Customer.objects.filter(username=username,password=password)
             if user:
-                return HttpResponse(request.kwargs)
+                return HttpResponse('dash board')
             else:
                 return HttpResponse('password or username is incorrect')
             
         else :
             return HttpResponse('no data')
+    else:
+        return HttpResponse('redirect to login')
